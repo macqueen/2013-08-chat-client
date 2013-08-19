48ad7c10def5c0c2c33fd2082e2db13b9336626c
Hack Reactor Students <github@hackreactor.com>
2013-08-19 16:05:07 -0700

    added typing area and got click handler working. need to push to parse db now

--- js/setup.js
+++ js/setup.js
@@ -7,28 +7,36 @@ if(!/(&|\?)username=/.test(window.location.search)){
   window.location.search = newSearch;
 }
 
-// Don't worry about this code, it will ensure that your ajax calls are allowed by the browser
-$.ajaxPrefilter(function(settings, _, jqXHR) {
-  jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
-  jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
+$(document).ready(function(){
+
+  // Don't worry about this code, it will ensure that your ajax calls are allowed by the browser
+  $.ajaxPrefilter(function(settings, _, jqXHR) {
+    jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
+    jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
+  });
+
+  //
+  var cleanMessages = function(msgArray) {
+    for (var i = 0; i < msgArray.length; i++) {
+      var msgString = msgArray[i].text + " " + "created at: " + msgArray[i].createdAt.slice(0,10);
+      $('<div></div>').attr('class', 'message').text(msgString).appendTo('#main');
+    }
+  };
+
+  $.get('https://api.parse.com/1/classes/messages', function(data) {
+    var jsonObj;
+    var msgArray = data.results;
+    cleanMessages(msgArray);
+  });
+
+  var newMessage = {
+    'username': 'shawndrost',
+    'text': 'trololo',
+    'roomname': '4chan' // Optional
+  };
+
+  $('#submitMsg').on('click', function(){
+    console.log("i got clicked!");
+    console.log($('.textBox').val());
+  });
 });
-
-//
-var cleanMessages = function(msgArray) {
-  for (var i = 0; i < msgArray.length; i++) {
-    var msgString = msgArray[i].text + " " + "created at: " + msgArray[i].createdAt.slice(10);
-    $('#main').append("<div>" + msgString + "</div>");
-  }
-};
-
-$.get('https://api.parse.com/1/classes/messages', function(data) {
-  var jsonObj;
-  var msgArray = data.results;
-  cleanMessages(msgArray);
-});
-
-var newMessage = {
-  'username': 'shawndrost',
-  'text': 'trololo',
-  'roomname': '4chan' // Optional
-};
