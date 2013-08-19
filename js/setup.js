48a1d64742eabebbd84dd689704d735e2ec911b3
Hack Reactor Students <github@hackreactor.com>
2013-08-19 15:16:53 -0700

    got messages appearing.

--- js/setup.js
+++ js/setup.js
@@ -12,3 +12,23 @@ $.ajaxPrefilter(function(settings, _, jqXHR) {
   jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
   jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
 });
+
+//
+var cleanMessages = function(msgArray) {
+  for (var i = 0; i < msgArray.length; i++) {
+    var msgString = msgArray[i].text + " " + "created at: " + msgArray[i].createdAt.slice(10);
+    $('#main').append("<div>" + msgString + "</div>");
+  }
+};
+
+$.get('https://api.parse.com/1/classes/messages', function(data) {
+  var jsonObj;
+  var msgArray = data.results;
+  cleanMessages(msgArray);
+});
+
+var newMessage = {
+  'username': 'shawndrost',
+  'text': 'trololo',
+  'roomname': '4chan' // Optional
+};
